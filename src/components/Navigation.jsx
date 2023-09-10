a02723b34fe50da4c2ad17ef95d841e3362875bf
mfried95 <m.friedman1995@gmail.com>
2023-09-10 12:28:22 -0400

    completed navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import logo from "../assets/SDM-logo.png";
+import "../styles/navigation.css";
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <>
+      <div className="nav-container">
+        
+        <div className="navlinks">
+          <img src={logo} alt="" />
+          <ul>
+            <li className="page-links">Trading</li>
+            <li className="page-links">Account</li>
+            <li className="page-links">History</li>
+          </ul>
+        </div>
 
-export default Navigation
+        <div className="nav-info">
+          <ul>
+            <li>%P&L</li>
+            <li>Equity</li>
+            <li>Deposit</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navigation;
