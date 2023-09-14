7afaa8feaf5b6067e7825d8005fe201f50814e69
Mfried95 <m.friedman1995@gmail.com>
2023-09-14 13:23:59 -0400

    Added state in action, changing the currency pair on selection

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -5,7 +5,10 @@ import { tradeOrders } from "../data/data";
 
 const Trade = () => {
   const [orders, setOrders] = useState([]);
+  const [currency, setCurrency] = useState('USD');
 
+  const currencies = ['USD', 'EUR', 'GBP', 'CAD', 'BTC', 'USDC', 'ETH'];
+  
   console.log(orders);
 
   useEffect(() => {
@@ -17,21 +20,18 @@ const Trade = () => {
       <header>
         <h1>Spot Trading</h1>
         <span className="slogan">
-          Discover SDM spot trading: deposit & trade today!{" "}
+          Discover SDM spot trading: deposit & trade today!
         </span>
       </header>
 
       <section className="currency-selection">
         <ul>
-          <li>USD</li>
-          <li>EUR</li>
-          <li>GBP</li>
-          <li>CAD</li>
-          <li>BTC</li>
-          <li>USDC</li>
-          <li>ETH</li>
-          <p> | you are trading with USDC</p>
+          {currencies.map((curr) => (
+            <li onClick={() => setCurrency(curr)} key={curr}>{curr}</li>
+          ))}
+          <p>| you are trading with {currency}</p>
         </ul>
+        
       </section>
 
       <section className="order-grid-container">
